51bd920142e59c7e5bd4addbeb7f84689748fd71
Leo Gavilieau <xmoo@privacyrequired.com>
2023-05-30 20:04:24 +0200

    tests: Write some more tests! 1. getTotalPosts(), getAdmins(), getTotalLocalUsers() all have tests now. (Enabled by default) 2. There is a test for getLocalPosts() but it makes the screen ugly. I could add a compile-time directive such as fullTests to enable (ehhh maybe later)
    Signed-off-by: Leo Gavilieau <xmoo@privacyrequired.com>

--- tests/test_conf.nim
+++ tests/test_conf.nim
@@ -9,7 +9,7 @@ for x in requiredConfigOptions:
   exampleConfig.add("\n[" & x.split(":")[0] & "]\n")
   exampleConfig.add(x.split(":")[1] & "=\"Test value\"\n")
 
-exampleConfig.add("[db]\nfilename=\"main.db\"")
+exampleConfig.add("\n[db]\nfilename=\"main.db\"")
 
 echo exampleConfig
 
--- tests/test_db.nim
+++ tests/test_db.nim
@@ -20,3 +20,44 @@ when not defined(iHaveMyOwnStuffThanks):
   for x in getFakePosts():
     discard db.addPost(escape(x))
 
+stdout.write "\nTesting getTotalPosts() "
+assert getTotalPosts() == len(fakeStatuses)
+stdout.write "Pass! \n"
+
+#[ Uncomment this if you want, I guess?
+echo "Displaying local Posts"
+  for x in getLocalPosts(0):
+  stdout.write("\n---\n")
+  echo "From: @", x.sender
+  if isEmptyOrWhitespace(x.replyto):
+    echo "To: Public"
+  else:
+    var printOut: string = ""
+    for user in x.recipients:
+      printOut.add("@" & user)
+    echo "To: ", printOut
+  echo "\n" & x.content
+  stdout.write("\n")
+]#
+
+stdout.write "Testing getAdmins() "
+# Create a new admin user
+var adminuser = newUser("johnadminson","John Adminson","123",true,true)
+adminuser.bio = "I am John Adminson! The son of the previous admin, George Admin"
+adminuser.email = "johnadminson@adminson.family.testinternal" # inb4 Google creates a testinternal TLD
+discard db.addUser(escape(adminuser))
+
+var adminFlag = false # This flag will get flipped when it sees the name "johnadminson" in the list of names that getAdmins() provides. If this happens then the test passes!
+for handle in getAdmins():
+  if handle == adminuser.handle:
+    adminFlag = true
+    break
+
+assert adminFlag == true
+stdout.write "Pass!\n"
+
+stdout.write "Testing getTotalLocalUsers() "
+# By this point we have added the fakeUsers + our fake admin user above.
+# So let's just test for this:
+assert getTotalLocalUsers() > len(fakeHandles)
+stdout.write "Pass!\n"
