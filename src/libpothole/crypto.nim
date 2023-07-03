f58d9785a39db0d3c30a6bba0e21d3f0912f960b
Leo Gavilieau <xmoo@privacyrequired.com>
2023-07-03 12:39:45 +0200

    docs: Document crypto.nim better
    Signed-off-by: Leo Gavilieau <xmoo@privacyrequired.com>

--- src/libpothole/crypto.nim
+++ src/libpothole/crypto.nim
@@ -15,12 +15,11 @@
 #
 # crypto.nim: 
 ## This file provides functions related to cryptography.
-## It provides the hash() function which is used to hash passwords and
-## it also provides lots of functions for generating random numbers, characters
-## and strings.
+## It provides wrapper functions for hashing, and it also provides several procedures to generate random numbers and strings.
+## It would be nice if this was included in the standard library but we unfortunately have to rely on a third-party library and an experimental unauditted standard library (std/sysrand)
 ## 
 ## I am certain that this module is cryptographically secure
-## But of course, feel free to audit it yourself.
+## but of course, feel free to audit it yourself.
 
 import nimcrypto
 import nimcrypto/pbkdf2
@@ -29,26 +28,11 @@ from std/base64 import encode
 from std/sysrand import urandom
 from std/strutils import parseInt
 
-#! std/sysrand has not been evaluated or auditted. But read the below notice before modifying.
-#[
-  Previously this file used std/random's rand() function to generate a number and then that would
-  be fed to a char function to create a string. This function was used to create strings for
-  Post ids, User ids and password salts. But the std/random RNG is not cryptographically
-  secure, it is possible to get the same output by simply running the program again.
-
-  I feel like I don't need to tell you why that's a bad thing, the whole point of the salt is that it is
-  somewhat obscure, but with std/random it was possible to just predict what salt would be next.
-  Our password hash function is somewhat secure and so it wouldn't be as big of a deal but I still didn't
-  like the fact that I would always get the same result no matter what.
-
-  The moral of the story is, while other languages have good cryptographic libraries, Nim has no sort of thing
-  like that, but I am okay with these risks so I will continue using nimcrypto and std/sysrand
-]#
-
 const asciiLetters: seq[char] = @[
   '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' 
 ] # This is basically Base62
-let maxASCIILetters* = len(asciiLetters) - 1
+
+let maxASCIILetters* = len(asciiLetters) - 1 ## The length of asciiLetters minus one.
 
 proc randomInt*(limit: int = 18): int =
   ## A function that generates an integer, limit is how many digits should be
@@ -94,7 +78,7 @@ proc randomString*(limit: int = 16): string =
 
 proc pbkdf2_hmac_sha512_hash*(password: string, salt:string, iter: int = 210000, outlen: int = 32, genSafe:bool = true): string =
   ## We use PBKDF2-HMAC-SHA512 by default with 210000 iterations unless specified.
-  ## This procedure is a wrapper for nimcrypto's PBKDF2 implementation
+  ## This procedure is a wrapper over nimcrypto's PBKDF2 implementation
   ## This procedure returns a base64-encoded string. You can specify the safe parameter of 
   ## encode() with the genSafe parameter
   runnableExamples:
