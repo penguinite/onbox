1e9bc830bf62cb331235215020bced3dd70b5a4a
Leo Gavilieau <xmoo@privacyrequired.com>
2023-11-11 16:29:35 +0100

    feat: use Tables for reactions
    Signed-off-by: Leo Gavilieau <xmoo@privacyrequired.com>

--- potholepkg/post.nim
+++ potholepkg/post.nim
@@ -24,18 +24,12 @@ import lib, crypto
 
 # From Nim's standard library
 import std/strutils except isEmptyOrWhitespace
-import std/times
+import std/[tables, times]
 
 export DateTime, parse, format, utc
 
 # ActivityPub Object/Post
 type
-
-  # Generic object for storing Boosts and Likes.
-  Action* = object
-    actor*: string # The actor's id
-    action*: string # The actor's action (Specific emote they react with, specific boost function they use.)
-
   Post* = object
     id*: string # A unique id.
     recipients*: seq[string] # A sequence of recipient's handles.
@@ -46,73 +40,9 @@ type
     updated*: DateTime # A timestamp of when then Post was last edited
     modified*: bool # A boolean indicating whether the Post was edited or not.
     local*:bool # A boolean indicating whether or not the post came from the local server or external servers
-    favorites*: seq[Action] # A sequence of reactions this post has.
-    boosts*: seq[Action] # A sequence of id's that have boosted this post.
+    favorites*: Table[string, string] # A sequence of reactions this post has.
+    boosts*: Table[string, string] # A sequence of id's that have boosted this post.
     revisions*: seq[string] # A sequence of past revisions, this is basically copies of post.content
-
-func escape*(obj: Action): Action =
-  ## A function to escape a Favorite object
-  result.actor = escape(obj.actor)
-  result.action = escape(obj.action)
-  return result
-
-proc escape*(post: Post): Post =
-  ## A procedure to escape a Post object
-  result = post
-
-  for key,val in result.fieldPairs:
-    when typeof(val) is bool:
-      result.get(key) = val
-
-    when typeof(val) is string:
-      result.get(key) = escape(val)
-
-    when typeof(val) is seq[string]:
-      var newseq: seq[string] = @[]
-      for x in val:
-        newseq.add(escape(x))
-      result.get(key) = newseq
-
-
-    when typeof(val) is seq[Action]:
-      var newseq: seq[Action] = @[]
-      for reaction in val:
-        newseq.add(escape(reaction))
-      result.get(key) = newseq
-
-  return result
-
-func unescape*(obj: Action): Action =
-  result.action = unescape(obj.action,"","")
-  result.actor = unescape(obj.actor,"","")
-  return result
-
-proc unescape*(post: Post): Post =
-  ## A procedure to unescape a Post object
-  result = post
-
-  # TODO: Look into using templates or macros to automatically
-  #       generate the loop that unescapes Posts
-  for key,val in result.fieldPairs:
-    when typeof(val) is bool:
-      result.get(key) = val
-
-    when typeof(val) is string:
-      result.get(key) = unescape(val,"","")
-
-    when typeof(val) is seq[string]:
-      var newseq: seq[string] = @[]
-      for x in val:
-        newseq.add(unescape(x,"",""))
-      result.get(key) = newseq
-
-    when typeof(val) is seq[Action]:
-      var newseq: seq[Action] = @[]
-      for x in val:
-        newseq.add(unescape(x))
-      result.get(key) = newseq
-
-  return result
 
 proc newPost*(sender,replyto,content: string, recipients: seq[string] = @[], local: bool = false, written: DateTime = now().utc, contexts: seq[string] = @[]): Post =
   var post: Post = Post()
@@ -147,53 +77,34 @@ func `$`*(obj: Post): string =
   result = result[0 .. len(result) - 2]
   result.add("]")
 
-proc create*(actor, action: string): Action =
-  ## Used in debug.nim and also the fromString() proc
-  result.actor = actor
-  result.action = action
+proc toString*(table: Table[string, string]): string =
+  for key,val in table.pairs:
+    result.add(escape(key) & ":" & escape(val) & ";")
   return result
 
-proc fromString*(str: string): seq[Action] = 
-  # Converts a string representation of an Action sequence which looks like this: 
-  # `"pyro":"sad";"heavy":"favorite";"medic":"happy"`
-  # into an actual sequence of Actions
+proc toTable*(str: string): Table[string, string] =
   var
-    actor = ""
-    action = ""
-    inStr = false
-    switchFlag = false
+    key,val = ""
+    strFlag, switch = false;
 
   for ch in str:
     if ch == '"':
-      if inStr:
-        inStr = false
+      if strFlag:
+        strFlag = false
       else:
-        inStr = true
-      continue
-
-    if not inStr:
+        strFlag = true
+    
+    if not strFlag:
       if ch == ':':
-        switchFlag = true
-      
-      if ch == ';':
-        result.add(create(actor, action))
-        actor = ""
-        action = ""
-        switchFlag = false
-      continue
-  
-    if switchFlag:
-      action.add(ch)
-    else:
-      actor.add(ch)
-  
-  return result
+        switch = true
+        continue
 
-proc toString*(sequence: seq[Action]): string =
-  for x in sequence:
-    result.add(x.actor & " -> " & x.action & "; ")
-  if len(sequence) > 0:
-    result = result[0 .. ^3]
+      if ch == ';':
+        result[unescape(key)] = unescape(val)
+    
+    if switch: val.add(ch)
+    else: key.add(ch)
+  
   return result
 
 proc toString*(sequence: seq[string]): string =
