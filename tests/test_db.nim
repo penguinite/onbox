67c934350c4aa2059677db03661bcb5b68ac57ff
Leo Gavilieau <xmoo@privacyrequired.com>
2023-12-04 17:01:21 +0100

    wip(tests): Some work on database testing
    Signed-off-by: Leo Gavilieau <xmoo@privacyrequired.com>

--- tests/test_db.nim
+++ tests/test_db.nim
@@ -1,2 +1,20 @@
-import potholepkg/database
+import potholepkg/[database, conf]
 
+var exampleConfig = ""
+
+for section, preKey in requiredConfigOptions.pairs:
+  exampleConfig.add("\n[" & section & "]\n")
+  for key in preKey:
+    exampleConfig.add(key & "=\"Test value\"\n")
+
+exampleConfig.add """
+[db]
+host="127.0.0.1:5432"
+name="pothole"
+user="pothole"
+password="SOMETHING_SECRET"
+"""
+
+let
+  config = setupInput(exampleConfig)
+  db = init(config)
