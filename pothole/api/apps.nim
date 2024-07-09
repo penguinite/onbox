753d1bb595edb8532dc34b8e6e01aad0ceffd307
penguinite <penguinite@tuta.io>
2024-07-09 13:38:35 +0200

    fix: Add a JSON counterpart to /api/v1/apps
    So apparently, some apps (cough, cough ihabunek's toot client, cough, cough) send a POST request with a JSON body instead of using multipart like sane people.
    
    Note to future self: POST with form parameters either means post with form parameters or POST with JSON body.
    
    I don't know who to blame, the individual mastodon clients or the api docs itself for not being clear.
    Either way, this *has* to be fixed because it's embarrasing.

--- pothole/api/apps.nim
+++ pothole/api/apps.nim
@@ -30,7 +30,8 @@ proc v1AppsMultipart*(req: Request) =
   var headers: HttpHeaders
   headers["Content-Type"] = "application/json"
 
-  var result: JsonNode
+  var
+    result: JsonNode
 
   # First, Check if client_name and redirect_uris exist.
   # If not, then error out.
@@ -45,7 +46,10 @@ proc v1AppsMultipart*(req: Request) =
   
   var
     client_name = mp.getFormParam("client_name")
-    redirect_uris = mp.getFormParam("redirect_uris") # TODO: What does one do with this???
+    # TODO: What does one do with this???
+    # The API docs suggest that we should parse and see if its an absolute uri.
+    # Throwing an error if it isn't... But like, what's the point of this anyway???
+    redirect_uris = mp.getFormParam("redirect_uris") 
 
   # Parse scopes
   var scopes = "read"
@@ -78,6 +82,98 @@ proc v1AppsMultipart*(req: Request) =
 
   req.respond(200, headers, $(result))
 
+proc hasValidStrKey(j: JsonNode, k: string): bool =
+  if not j.hasKey(k):
+    return false
+
+  if j.kind != JString:
+    return false
+
+  try:
+    if j[k].getStr().isEmptyOrWhitespace():
+      return false
+  except:
+    return false
+
+  return true
+
+
+proc v1AppsJSON*(req: Request) =
+  var headers: HttpHeaders
+  headers["Content-Type"] = "application/json"
+
+  var json: JsonNode = newJNull()
+  try:
+    json = parseJSON(req.body)
+  except:
+    req.respond(401, headers, $(%*{"error": "Invalid JSON."}))
+    return
+
+  # Double check if the parsed JSON is *actually* valid.
+  if json.kind == JNull:
+    req.respond(401, headers, $(%*{"error": "Invalid JSON."}))
+    return
+
+  # First, Check if client_name and redirect_uris exist.
+  # If not, then error out.
+
+  if not json.hasValidStrKey("client_name") or not json.hasValidStrKey("redirect_uris"):
+    req.respond(401, headers, $(%*{"error": "Missing required parameters."}))
+    return
+
+  var website = ""
+  if json.hasValidStrKey("website"):
+    website = json["website"].getStr()
+  
+  var
+    client_name = json["client_name"].getStr()
+    # TODO: What does one do with this???
+    # The API docs suggest that we should parse and see if its an absolute uri.
+    # Throwing an error if it isn't... But like, what's the point of this anyway???
+    redirect_uris = json["redirect_uris"].getStr()
+
+  # Parse scopes
+  var scopes = "read"
+  if json.hasValidStrKey("scopes"):
+    for scope in json["scopes"].getStr().split(" "):
+      if not scope.verifyScope():
+        req.respond(401, headers, $(%*{"error": "Invalid scope: " & escape(scope)}))
+        return
+    scopes = json["scopes"].getStr()
+  
+  var client_id, client_secret: string
+  dbPool.withConnection db:
+    client_id = db.createClient(
+      client_name,
+      website,
+      scopes
+    )
+    client_secret = db.getClientSecret(client_id)
+  
+  var result = %* {
+    "id": client_id,
+    "name": client_name,
+    "website": website,
+    "redirect_uri": redirect_uris,
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "scopes": scopes.split(" ") # Undocumented.
+  }
+
+  req.respond(200, headers, $(result))
+
+proc v1Apps*(req: Request) =
+  # Switch between multipart and JSON 
+  # if req.unrollMultipart() fails.
+  try:
+    discard req.unrollMultipart()
+    # If we are still here then it means the 
+    # request uses multipart.
+    req.v1AppsMultipart()
+  except:
+    req.v1AppsJSON()
+
+  
 proc v1AppsVerify*(req: Request) = 
   var headers: HttpHeaders
   headers["Content-Type"] = "application/json"
