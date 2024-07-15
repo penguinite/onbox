bf3c7db71029ab2902ce10bb4ab108ecfdabe997
penguinite <penguinite@tuta.io>
2024-07-15 16:33:16 +0200

    feat: Oauth authorization page!

--- pothole/assets.nim
+++ pothole/assets.nim
@@ -30,6 +30,7 @@ when not defined(phNoEmbeddedAssets):
     "about.html": staticRead("../assets/" & phLang & "/about.html"),
     "signup.html": staticRead("../assets/" & phLang & "/signup.html"),
     "signin.html": staticRead("../assets/" & phLang & "/signin.html"),
+    "oauth.html": staticRead("../assets/" & phLang & "/oauth.html"),
     "generic.html": staticRead("../assets/" & phLang & "/generic.html"),
     "style.css": staticRead("../assets/style.css") # CSS doesn't need language.
   }.toTable
