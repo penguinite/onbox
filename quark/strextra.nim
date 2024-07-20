e939f95c95a2c896424b2370aa645cf6d16535ac
penguinite <penguinite@tuta.io>
2024-07-20 09:27:15 +0200

    docs: Document this module

--- quark/strextra.nim
+++ quark/strextra.nim
@@ -1,3 +1,24 @@
+# Copyright © Leo Gavilieau 2022-2023 <xmoo@privacyrequired.com>
+# Copyright © penguinite 2024 <penguinite@tuta.io>
+#
+# This file is part of Pothole. Specifically, the Quark repository.
+# 
+# Pothole is free software: you can redistribute it and/or modify it under the terms of
+# the GNU Affero General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# 
+# Pothole is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with Pothole. If not, see <https://www.gnu.org/licenses/>. 
+#
+# quark/strextra.nim:
+## This module provides complementary string handling functions
+## and it also provides faster alternatives to the ones in std/strutils
+
 from std/strutils import Whitespace, `%`, toLowerAscii, startsWith
 
 func isEmptyOrWhitespace*(str: string, charset: set[char] = Whitespace): bool =
