de082c7c9b2e793089b2ee5867ce2405547d976d
penguinite <penguinite@tuta.io>
2025-03-31 15:50:55 +0200

    fix: Implement flat-storage for getAvatar and getHeader
    These haven't been tested yet...

--- src/pothole/assets.nim
+++ src/pothole/assets.nim
@@ -33,13 +33,13 @@ proc getAvatar*(config: ConfigTable, user_id: string): string =
   case config.getString("storage", "type"):
   of "flat":
     # Simply find the file in the filesystem and return a public link to it.
-    discard
 
     # If `upload_uri` has been configured, then we'll use it as a base
     # Otherwise we'll use realURL() + /media/
     if config.exists("storage", "upload_uri"):
+      return config.getString("storage", "upload_uri") & "user/" & user_id & "/avatar.webp"
     else:
-      config.realURL() & "media/user/"
+      return config.realURL() & "media/user/" & user_id & "/avatar.webp"
 
   of "pony":
     # "pony" is a media pooling feature, similar in spirit to Jortage.
@@ -62,4 +62,34 @@ proc getAvatar*(config: ConfigTable, user_id: string): string =
     )
   
 proc getHeader*(config: ConfigTable, user_id: string): string =
-  return # TODO: Implement
+  ## When given a user's ID, return a URL to their avatar.
+  case config.getString("storage", "type"):
+  of "flat":
+    # Simply find the file in the filesystem and return a public link to it.
+
+    # If `upload_uri` has been configured, then we'll use it as a base
+    # Otherwise we'll use realURL() + /media/
+    if config.exists("storage", "upload_uri"):
+      return config.getString("storage", "upload_uri") & "user/" & user_id & "/header.webp"
+    else:
+      return config.realURL() & "media/user/" & user_id & "/header.webp"
+
+  of "pony":
+    # "pony" is a media pooling feature, similar in spirit to Jortage.
+    # This is an experimental feature as no reliable server exists for this
+    # kinda thing yet...
+    raise newException(
+      CatchableError,
+      "Pony pooling!(tm) Coming sooner or later, eventually..."
+    )
+  of "remote":
+    # TODO: Finish S3 compatability
+    raise newException(
+      CatchableError,
+      "S3 storage: Not implemented yet, sorry."
+    )
+  else:
+    raise newException(
+      CatchableError,
+      "Unknown media storage type: " & config.getString("storage", "type")
+    )
